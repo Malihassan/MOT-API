c2a48e5133393154f6f8458125a0cb1e9d855211
Your NameMalihassan <momenalhendawy@gmail.com>
2021-04-18 04:40:39 +0200

    adding Two Post Request 1-to adding new Sensor 2-to adding consolidated

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ async function CheckReadingsCONS(Name,Cookie) {
       console.log(response.data);
       // res.send(response.data)
       if(response.data.message === true){
-       addSen = await AddSensor(Name,Cookie)
+       addSen = await AddSensorCONS(Name,Cookie)
       }
     })
     .catch(function (error) {
@@ -169,7 +169,7 @@ async function CheckReadingsCONS(Name,Cookie) {
     });
     return addSen ;
 }
-async function AddSensor(Name,Cookie){
+async function AddSensorCONS(Name,Cookie){
   let sensorId ;
    let URL = 'https://learning.masterofthings.com/RegisterSensor';
   let config = {
@@ -282,86 +282,82 @@ const jsonrequest = {
     ]
   }
 }
-// app.post("/RegisterSensor", async (req, res) => {
-//   console.log("in addNewSensor router");
-//   const { Name } = req.body
-//   // console.log(`the Name of Sensor :${Name} , Cookis :${Cookis}`);
+app.post("/RegisterSensor", async (req, res) => {
+  console.log("in addNewSensor router");
+  const { Name } = req.body
+  // console.log(`the Name of Sensor :${Name} , Cookis :${Cookis}`);
 
-//   // this for get cookis 
-//   let URL = 'https://learning.masterofthings.com/Login'
-//   let data = 'username=momenzakaria&password=MOMEN@@2016&DomainName=Training'
-//   let config = {
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-//     }
-//   }
-//   axios.post(URL, data, config)
-//     .then( async function (response) {
-//       let totalcookie = response.headers["set-cookie"][0];
-//       // console.log("==========>" + response.headers["set-cookie"]);
-//       // console.log(typeof (response.headers["set-cookie"]));
-//       let splitcookie = totalcookie.split(";")
-//       let splitsplitcoo = splitcookie[0].split("=")
-//       // console.log(`the Name of Sensor :${Name} , Cookie :${splitsplitcoo[1]}`);
-//       // res.send(response.headers["set-cookie"]) 
-//       let result = await CheckReadings(Name ,splitsplitcoo[1]);
-//       console.log(result);
-//       res.send({"sensorId":result});
-//       // res.send(`the Name of Sensor :${Name} , Cookie :${splitsplitcoo[1]}`)
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// })
-// async function CheckReadings(Name,Cookie) {
-//   let addSen
-//   let URL = 'https://learning.masterofthings.com/CheckReadings'
-//   let config = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Cookie': 'MOTSID =' + Cookie
-//     }
-//   }
-//   let data = { "readings": ["appVersion", "phoneVersion", "captureTime", "currentNearnessLevel"] }
-//   await axios.post(URL, data, config)
-//     .then(async function (response) {
-//       console.log(response.data);
-//       // res.send(response.data)
-//       if(response.data.message === true){
-//        addSen = await AddSensor(Name,Cookie)
+  // this for get cookis 
+  let URL = 'https://learning.masterofthings.com/Login'
+  let data = 'username=momenzakaria&password=MOMEN@@2016&DomainName=Training'
+  let config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+  }
+  axios.post(URL, data, config)
+    .then( async function (response) {
+      let totalcookie = response.headers["set-cookie"][0];
+      // console.log("==========>" + response.headers["set-cookie"]);
+      // console.log(typeof (response.headers["set-cookie"]));
+      let splitcookie = totalcookie.split(";")
+      let splitsplitcoo = splitcookie[0].split("=")
+      // console.log(`the Name of Sensor :${Name} , Cookie :${splitsplitcoo[1]}`);
+      // res.send(response.headers["set-cookie"]) 
+      let result = await CheckReadingsSens(Name ,splitsplitcoo[1]);
+      console.log(result);
+      res.send({"sensorId":result});
+      // res.send(`the Name of Sensor :${Name} , Cookie :${splitsplitcoo[1]}`)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+})
+async function CheckReadingsSens(Name,Cookie) {
+  let addSen
+  let URL = 'https://learning.masterofthings.com/CheckReadings'
+  let config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': 'MOTSID =' + Cookie
+    }
+  }
+  let data = { "readings": ["appVersion", "phoneVersion", "captureTime", "currentNearnessLevel"] }
+  await axios.post(URL, data, config)
+    .then(async function (response) {
+      console.log(response.data);
+      // res.send(response.data)
+      if(response.data.message === true){
+       addSen = await AddSensor(Name,Cookie)
        
-//       }
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-//     return addSen ;
-// }
-// async function AddSensor(Name,Cookie){
-//   let sensorId ;
-//    let URL = 'https://learning.masterofthings.com/RegisterSensor';
-//   let config = {
-//   headers: {
-//   'Content-Type': 'application/json',
-//   'Cookie':'MOTSID ='+Cookie
-//           }
-//   }
-//   let data =jsonrequest;
-//   data.Package.SensorDetails.SensorName = Name;
-//   let x = await axios.post(URL, data, config)
-//   .then(function (response) {
-//         // console.log(response.data.SensorId);
-//          sensorId = response.data.SensorId;
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-//     return sensorId ;
-// }
-
-
-
-
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    return addSen ;
+}
+async function AddSensor(Name,Cookie){
+  let sensorId ;
+   let URL = 'https://learning.masterofthings.com/RegisterSensor';
+  let config = {
+  headers: {
+  'Content-Type': 'application/json',
+  'Cookie':'MOTSID ='+Cookie
+          }
+  }
+  let data =jsonrequest;
+  data.Package.SensorDetails.SensorName = Name;
+  let x = await axios.post(URL, data, config)
+  .then(function (response) {
+        // console.log(response.data.SensorId);
+         sensorId = response.data.SensorId;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    return sensorId ;
+}
 
 
 const port = process.env.PORT || 3000;
